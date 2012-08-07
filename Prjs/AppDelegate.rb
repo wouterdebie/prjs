2e00495cad4c2409e709fb5e1e28d9a4460c7be6
Wouter de Bie <wouter@spotify.com>
2012-08-07 15:56:27 +0200

    Initial Commit

---
+++ Prjs/AppDelegate.rb
@@ -0,0 +1,15 @@
+#
+#  AppDelegate.rb
+#  Prjs
+#
+#  Created by Wouter de Bie on 8/7/12.
+#  Copyright 2012 Wouter de Bie. All rights reserved.
+#
+
+class AppDelegate
+    attr_accessor :window
+    def applicationDidFinishLaunching(a_notification)
+        # Insert code here to initialize your application
+    end
+end
+
